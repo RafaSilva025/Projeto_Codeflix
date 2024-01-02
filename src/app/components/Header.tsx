63820942cf35905933167d43f2b935e4e036f842
Rafa <rafasilvasimoes25@gmail.com>
2024-01-02 01:23:00 -0300

    "Use Client"

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+"use client"
+import React, { useEffect, useState } from "react";
 import { Logo } from './Logo';
 import Link from "next/link";
 
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect (() =>{
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+    
+
+        window.addEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <header className="fixed top-0 z-50 flex w-full items-center justify-between transition-all bg-black px-4 py-4 lg:px-10 lg:py-6">
-            <div className="bg-red-200 flex items-center space-x-2 md:space-x-4">
+        <header className={`
+        ${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between transition-all bg-black px-4 py-4 lg:px-10 lg:py-6`}>
+            <div className="flex items-center space-x-2 md:space-x-4">
             <Link href='/'>
                 <Logo />
             </Link>
