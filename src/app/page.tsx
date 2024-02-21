44cbf2844f4023f6edd448ba1bdcb266c40dc032
Leoozito <leonardonogueira616@gmail.com>
2024-02-21 08:38:01 -0300

    [UPD]- colocando imagens na tela principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,17 +30,25 @@ export default function Home() {
         <button className='bg-gray-400 text-black md:text-xl; flex cursor-pointer items-center gap-x-2 rounded px-5 py-1.5 text-sm font-semibold transition hover:opacity-75 md:px-8 md:py-2.5'>More Info</button>
         </div>
         <div className='flex-col space-y-4'>
-          <div className='flex space-x-4 overflow-x-scroll'>
-            <div className='h-28 min-w-[200px]'>
-          {[1, 2, 3, 4, 5,].map((index) => (
+          <div className='flex'>
+            <h2 className='-ml-2 inline-flex items-center text-2xl font-bold'>Featured</h2>
+          </div>
+
+          <div className='-ml-8 flex space-x-4 overflow-x-scroll p-6 scrollbar-hide'>
+            {[1, 2, 3, 4, 5, 5, 5, 5].map((index) => (
+              <div
+              key={index}
+              className="group relative h-28 min-w-[200px] transform bg-gradient-to-t from-transparent to-black transition duration-200 ease-in hover:z-50 hover:scale-110 md:h-40 md:min-w-[300px] lg:h-52 lg:min-w-[400px]"
+              >
             <Image
               key={index}
               src={`/item_${index}.png`}
               fill={true}
               alt='MAID'
               className='rounded'
-          ))}
+              />
             </div>
+            ))}
           </div>
         </div>
         </main>
