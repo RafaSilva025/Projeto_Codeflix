ad77ef24ad1f3d86d7ba47961921dc69b32d4091
Leoozito <leonardonogueira616@gmail.com>
2024-02-22 08:41:36 -0300

    [UPD]- Page Children

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,12 @@
+import React from React; 
+
+function Settings () {
+    return (
+        <div>
+            <h1 className="text-2xl font-bold">Settings Page</h1>
+        </div>
+    );
+}
+
+
+export default Settings
