74edd01741c7567111340a65bc848a0d56243ddd
Leoozito <leonardonogueira616@gmail.com>
2024-02-27 15:04:32 -0300

    [UPD]- inicio da auth

--- src/app/lib/settings.tsx
+++ src/app/lib/settings.tsx
@@ -3,5 +3,23 @@ return new Promise((resolve) => {
     resolve({
         theme: 'dark',
         language: 'en',
+
+
+        export const getUserInfo = (): Promise<{
+            name: string;
+            email: string;
+            age: number;
+        }> => {
+            return new Promise((resolve) => {
+                name: 'John Doe',
+                email: 'John@.com'
+                age: 30,
+            });
+        }, 1000);
+        }
     });
 });
+
+export async function getUserById(id: string): Promise<User> => {
+    const response = await fetch
+}
