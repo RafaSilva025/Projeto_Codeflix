6847f9168e25c86b588872ffc0c0570ec954f27b
Rafa <rafasilvasimoes25@gmail.com>
2024-03-06 11:47:43 -0300

    [UPD]

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,9 +1,3 @@
-import { NextRequest, NextResponse } from "next/server";
-import { LoginValidation } from "../lib/LoginValidation";
-import LoginForm from "../auth/login/Login";
-
-export async function POST(request: NextRequest) {
-    const { email, password } = LoginForm.parse(await request.json());
-} catch (error) {
-    return NextResponse.badRequest(error.message);
-}
+export async function GET(request: Request) {
+    return new Response('Hello, Next.js!')
+  }
