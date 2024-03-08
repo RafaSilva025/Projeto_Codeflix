2f8574950f6c3e2f6448ade010089b7a7005deed
Rafa <rafasilvasimoes25@gmail.com>
2024-03-08 10:12:53 -0300

    [UPD]- service

--- src/app/service/ApiRequest.ts
+++ src/app/service/ApiRequest.ts
@@ -1,3 +1,26 @@
-
-
 const API_URL = 'http://localhost:3333'
+
+export interface ApiQueryParams {
+    [key: string]: string | number | boolean;
+}
+
+export interface RequestOptions{
+    page?: number;
+    _limit?: number;
+    rating_like?: string;
+}
+
+export const defaultRequestOptions: RequestOptions = {
+    page: 1,
+    _limit: 10,
+}
+
+export async function apiRequest(endpoint: string, query: ApiQueryParams = {}, options: RequestOptions ={}) {
+    try {
+    const response = fetch(`${API_URL}/${endpoint}');
+    const data = await (await response).json();
+    return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
