1c5c23a2120f4aab8d4f4df8d0cc1c85edfe11be
Rafa <rafasilvasimoes25@gmail.com>
2024-03-18 17:42:45 -0300

    [UPD]- MoviesService

--- src/app/service/MovieService.ts
+++ src/app/service/MovieService.ts
@@ -1,7 +1,34 @@
+import { Movie, Movies } from '../types/movie';
 import { RequestOptions, apiRequest } from './ApiRequest';
 
+export const getMovieById = async (id: string): Promise<Movie> => {
+  return apiRequest(`movies/${encodeURIComponent(id)}`);
+};
 
-export const getMovieById = async (id: string) => {
+export const getFeaturedMovie = async (id: string): Promise<Movie> => {
+  return apiRequest(`featured/${id}`);
+};
 
-    return await apiRequest(`movies/${id}`);
-}
+export const getMoviesByGenre = async (
+  genre: string,
+  options?: RequestOptions
+): Promise<Movies> => {
+  return apiRequest(`movies`, { genres_like: genre }, options);
+};
+
+export const searchMovies = async (
+  title: string = '',
+  genre: string = '',
+  options: RequestOptions = {
+    _limit: 100,
+  }
+): Promise<Movies> => {
+  return apiRequest(
+    `movies`,
+    {
+      title_like: title,
+      genres_like: genre,
+    },
+    options
+  );
+};
